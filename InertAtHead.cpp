b06e26758f41ba807fe692997b44318713ba9cd6
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2023-01-18 00:34:23 +0600

    main
    main

---
+++ InertAtHead.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+        int data;
+        node* next;
+
+        node(int value){
+            data = value;
+            next = NULL;
+        }
+};
+void InertAtHead(node* &head, int value){
+    node* n = new node(value);
+    n->next = head;
+    head = n;
+}
+void display (node* head){
+    node* temp = head;
+    while (temp!=NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main(){
+    node* head = NULL;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        InertAtHead(head,x);
+    }
+    display(head);
+    return 0;
+}
