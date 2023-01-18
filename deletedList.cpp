28f00fb013031744c3bc110c71c3890a1a2b20d5
Ra_Rafiq <119992536+RofiqulIsalm@users.noreply.github.com>
2023-01-18 21:57:48 +0600

    main
    main

---
+++ deletedList.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+        int data;
+        node* next;
+
+        node(int value){
+            data = value;
+            next = NULL;
+        }
+};
+void deletionAtHead(node* &head){ // deleted at head
+    node* temp = head;
+    head = head->next;
+    delete temp;
+}
+void deletion(node* &head, int vlaue){ // deleted at tail
+    node* temp = head;
+    while (temp->next->data!=NULL)
+    {
+        temp = temp->next;
+    }
+    node* todeleted = temp;
+    temp->next = temp->next->next;
+
+    delete todeleted;
+}
